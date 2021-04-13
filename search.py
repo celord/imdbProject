2931e3036e5b8556ae28f43f682030d740dd5b11
Cesar Garcia <celord@gmail.com>
2021-04-13 10:01:07 -0600

    Implementatio of search_by_name method

--- search.py
+++ search.py
@@ -1,29 +1,31 @@
-#from movies import Movies
 import re
 
 def search_by_name(name,moviedb):
 
-    #name = [x for x in name.split()]
+    mv = []
+    name = [x for x in name.split()]
     for i in moviedb:
         actor_director = [ x.lower() for x in moviedb[i]['director_actor']]
-        #print("*************************")
-        #print(name)
-        #print(actor_director)
-        #if len(name) > 1:
-        #    pass
-        #else:
-        #    print('*************************************')
-        for j in actor_director:
-            pattern = re.compile('.*' + name.lower() + '.*')
-            #print(pattern)
-            if re.findall(pattern,j):
-                print(moviedb[i])
-        
 
-#
-#if __name__ == '__main__':
-#    #db = Movies()
-#    #movies = db.get_top_movies()
-#
-#    search_by_name('spielberg',movies)
-#
+        if len(name) > 1:
+            #print(name)
+            #print(actor_director)
+            itr = []
+            for ad in actor_director:
+                for n in name:
+                    if re.findall('.*' + n.lower() + '.*', ad):
+                        itr.append(True)
+                    else:
+                        pass
+            
+            if len(name) == len(itr):
+                if all(itr):
+                    mv.append(moviedb[i]['movie_title'])
+                    
+
+        else:
+            for j in actor_director:
+                if re.findall('.*' + name[0].lower() + '.*', j):
+                    mv.append(moviedb[i]['movie_title'])
+    
+    return(mv)
